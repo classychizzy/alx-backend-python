707fcb0b5f526d30ca3bf38ebd189538fe07b300
classychizzy <chizobachukwuoma@gmail.com>
2023-01-05 22:54:37 +0000

    fixed type bug

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,7 +7,7 @@ to a list
 from typing import List
 
 
-def sum_list(input_list: List[float]):
+def sum_list(input_list: List[float]) -> float:
     """
     a function that returns the
     sum of a list
