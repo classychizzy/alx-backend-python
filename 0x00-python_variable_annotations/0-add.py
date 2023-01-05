b26aa56f431ad9c392f4cb89cc5949a29208c013
classychizzy <chizobachukwuoma@gmail.com>
2023-01-05 20:07:36 +0000

    fixed bug

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 """
-a Program that adds to numbers 
+a Program that adds to numbers
 with annotated types
 """
 
-def add (a: float, b: float) -> float:
+
+def add(a: float, b: float) -> float:
     """
     a function that adds two floats
     """
-    return  a + b
-
+    return a + b
