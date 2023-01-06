50b554ce639973281ab739c502ffedcde24da37e
classychizzy <chizobachukwuoma@gmail.com>
2023-01-06 20:36:23 +0000

    fixed problem with type

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,11 +6,11 @@ a program that returns a tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     a function that returns a tuple of
     a string and a number
     """
-    square = v * v
+    square: float = v * v
     tup1 = (k, square)
     return tup1
