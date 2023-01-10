a1300a04b92a7937243018d00edffddbc9df9ac6
classychizzy <chizobachukwuoma@gmail.com>
2023-01-10 15:26:31 +0000

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+a program that uses async comprehension
+"""
+
+
+import asyncio
+import random
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    a coroutine that uses async comprehension
+    """
+    return [i async for i in async_generator()]
